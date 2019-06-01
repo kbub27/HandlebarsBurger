0e1b86b6ad47f722f92e2e215c03a7fbf5cbe1fd
kbub27 <kalebmerriman27@gmail.com>
2019-06-01 09:57:25 -0700

    got conversion functions created in the orm

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,45 @@
-var Connection = require('./connection.js');
+var Con = require('./connection.js');
+
+// FUNCTION TO GET QUESTION MARKS INTO A STRING
+var printQs = (num) => {
+    var arr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        arr.push('?');
+    }
+
+    return arr.toString();
+};
+
+// FUNCTION TO CONVERT OBJECT PAIRS TO SQL SYNTAX
+var o2s = (ob) => {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            
+            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+                value = "'" + value + "'";
+            }
+        }
+    }
+
+    return arr.toString();
+}
 
 module.exports = {
     
-    selectAll: (app) => {
+    selectAll: (input, cb) => {
+        
+    },
+
+    insertOne: (table, cols, vals, cb) => {
 
     },
 
-    insertOne: (app) => {
-
-    },
-
-    updateOne: (app) => {
+    updateOne: (table, objColVals, condition, cb) => {
         
     }
 }
