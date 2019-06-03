4b35ae3ead056036d1cf1efb48247acdadd9db48
kbub27 <kalebmerriman27@gmail.com>
2019-06-02 20:47:52 -0700

    burger model object is set up

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,21 @@
 var Orm = require('../config/orm.js');
 
-module.exports = (app) => {
-    
-}
+module.exports = {
+    all: (cb) => {
+        Orm.selectAll('burgers', (res) => {
+            cb(res);
+        });
+    },
+
+    create: (cols, vals, cb) => {
+        Orm.insertOne('burgers', cols, vals, (res) => {
+            cb(res);
+        });
+    },
+
+    update: (objColVals, condition, cb) => {
+        Orm.updateOne('burgers', objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+};
